c5b9d7c8ead70407e1336128723d4de336e201cd
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-07-07 17:26:29 +0300

    The translator now can support 13 languages, instead of just english and french

--- code.py
+++ code.py
@@ -4,28 +4,51 @@ from bs4 import BeautifulSoup
 
 # Constants
 ORIGINAL_URL = 'https://context.reverso.net/translation/'
-LANGUAGE_LOOKUP = {'fr': 'french', 'en': 'english'}
+LANGUAGE_LOOKUP = {'1': 'arabic',
+                   '2': 'german',
+                   '3': 'english',
+                   '4': 'spanish',
+                   '5': 'french',
+                   '6': 'hebrew',
+                   '7': 'japanese',
+                   '8': 'dutch',
+                   '9': 'polish',
+                   '10': 'portuguese',
+                   '11': 'romanian',
+                   '12': 'russian',
+                   '13': 'turkish'}
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
-# Read user input
+# Printing the welcoming message showcasing the languages this translator can
+# support
+print('Hello, welcome to the translator. Translator supports:')
+for key, value in LANGUAGE_LOOKUP.items():
+    print(f'{key}. {value.title()}')
+
+# Take the language the user wants to translate from
 while True:
-    print('Type "en" if you want to translate from French into English, or \
-"fr" if you want to translate from English into French:')
-    lang_to = input()
-    if lang_to == 'fr':
-        lang_from = 'en'
-        break
-    elif lang_to == 'en':
-        lang_from = 'fr'
-        break
+    print('Type the number of your language:')
+    lang_from = input()
+    if lang_from not in LANGUAGE_LOOKUP:
+        print('Incorrect input.')
     else:
-        print('Incorrect Format')
+        break
 
-# Take the word to be translated from the user
+# Take the language the user wants to translate to
+while True:
+    print('Type the number of language you want to translate to:')
+    lang_to = input()
+    if lang_to not in LANGUAGE_LOOKUP:
+        print('Incorrect input.')
+    elif lang_to == lang_from:
+        print('This should be different from the original language.')
+    else:
+        break
+
+
 print('Type the word you want to translate:')
-word_to_translate = input()
-
-print(f'You chose {lang_to} as the language to translate {word_to_translate}.')
+word_to_translate = input().lower()
+print()
 
 # Create teh website url from which the translation will be extracted
 website_url = ORIGINAL_URL + \
@@ -35,9 +58,6 @@ website_url = ORIGINAL_URL + \
 # Get the request from the website and extract its content
 response = requests.get(website_url, headers=HEADERS)
 soup = BeautifulSoup(response.content, 'html.parser')
-
-if response:
-    print(response.status_code, 'OK', end='\n\n')
 
 # Getting the translations to the required word
 word_translations = soup.find_all('span', {'class': 'display-term'})
